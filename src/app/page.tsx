941133d68eeb8a0b44bb0cdc912570832ac3115d
Joyce Chen <joyce.chen721@gmail.com>
2023-07-23 23:28:27 +0800

    move components into src/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import Image from "next/image";
-import NavBar from "./components/NavBar";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import NavBar from "../components/NavBar";
+import MainContent from "../components/MainContent";
+import Footer from "../components/Footer";
 
 // <main className="flex min-h-screen flex-col items-center justify-between p-24">
 // </main>
